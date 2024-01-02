76161524646651ba73f30a01da155cabbec38076
asoto225 <abelsoto12@hotmail.com>
2024-01-02 15:23:33 -0700

    fixed port

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
-// const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 const server = new ApolloServer({
   typeDefs,
@@ -34,11 +34,10 @@ const startApolloServer = async () => {
   };
 
   db.once('open', () => {
-    // app.listen(PORT, () => {
-    //   console.log(`API server running on port ${PORT}!`);
-    //   console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
-    // });
-    console.log('db connected');
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+    });
   });
 };
 
